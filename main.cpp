3b880e4a36d00d9d503b2117df6f209c8061e551
Will Becker <willvfs@gmail.com>
2024-04-15 23:38:37 -0700

    Brought PINE into the project

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "pine.h"
 
 int main(int, char**){
-    std::cout << "Hello, from rac2_client!\n";
+    PINE::PCSX2 *ipc = new PINE::PCSX2();
+    
+    try {
+        std::cout << "PCSX2 version: " << ipc->Version() << "\n";
+    } 
+    catch (PINE::Shared::IPCStatus error) {
+        if (error == PINE::Shared::IPCStatus::NoConnection) {
+            std::cout << "Failed to get PCSX2 version - Cannot connect to the IPC socket.\n";
+        }
+        else {
+            throw error;
+        }
+    }
+    
 }
