063664f8bae062ca316a83c3e0f6248f0e45b976
evilwb <willvfs@gmail.com>
2024-05-21 00:33:23 -0700

    Started on AP server connection

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
-#include <unistd.h>
+#include "main.h"
 
-#include <chrono>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <vector>
-
-#include "APCpp/Archipelago.h"
-#include "pine/pine.h"
-#include "rac2_interface.h"
+#include "Archipelago.h"
 #include "readerwriterqueue.h"
-#include "util.h"
 
 using namespace std;
 using namespace PINE;
@@ -19,8 +9,8 @@ using namespace moodycamel;
 using namespace Rac2;
 
 int main(int, char **) {
-    ReaderWriterQueue<string> input_queue(20);
-    thread input_reader([&]() {
+    input_queue = ReaderWriterQueue<string>(20);
+    input_reader = thread([&]() {
         while (true) {
             string input;
             getline(cin, input);
@@ -29,10 +19,10 @@ int main(int, char **) {
     });
     input_reader.detach();
 
-    unique_ptr<Rac2_interface> rac2 = make_unique<Rac2_interface>();
+    rac2 = make_unique<Rac2_interface>();
 
-    bool rac2_connected = false;
-    bool ap_connected = false;
+    rac2_connected = true;
+    ap_connected = false;
 
     while (true) {
         // Connect to PCSX2
@@ -61,19 +51,45 @@ int main(int, char **) {
         // Proccess Input
         string input;
         if (input_queue.try_dequeue(input)) {
-            if (input.front() == '/') {
-                cout << "client command\n> " << flush;
-                // TODO: handle client commands
-            } else if (input.front() == '!') {
-                cout << "server command\n> " << flush;
-                // TODO: handle server commands
+            vector<string> tokens = tokenize(input);
+
+            if (tokens[0] == "/help") {
+                // TODO: print command help
+                cout << "Command help coming soon `\\_( ͡° ͜ʖ ͡°)_/¯\n> " << flush;
+            } else if (tokens[0] == "/connect" && tokens.size() == 3) {
+                ap_connected = ap_connect(tokens[1], tokens[2]);
+            } else if (tokens[0] == "connect" && tokens.size() == 4) {
+                ap_connected = ap_connect(tokens[1], tokens[2], tokens[3]);
             } else {
-                cout << input << "\n> " << flush;
-                // TODO: handle say
+                cout << "Invalid command. Try using '/help' for a list of "
+                        "valid commands\n> "
+                     << flush;
             }
         }
 
         sleep(1);
     }
-    // delete inv;
+}
+
+bool ap_connect(const std::string address, const std::string slotname,
+                const string password) {
+    // if (!AP_IsInit()) {
+        AP_Init(address.c_str(), "RAC2", slotname.c_str(), password.c_str());
+
+        AP_SetItemClearCallback([]() {
+            // TODO: Clear all item states
+        });
+
+        AP_SetItemRecvCallback([](int64_t item_id, bool notify) {
+            // TODO: add the item with the given ID to the current state. The
+            // second parameter decides whether or not to notify the player
+        });
+
+        AP_SetLocationCheckedCallback([](int64_t location_id) {
+            // TODO: mark the location with the given id as checked.
+        });
+    // }
+
+    AP_Start();
+    return false;
 }
---
+++ main.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <unistd.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "APCpp/Archipelago.h"
+#include "pine/pine.h"
+#include "rac2_interface.h"
+#include "readerwriterqueue.h"
+#include "util.h"
+
+bool ap_connect(const std::string address, const std::string slotname, const std::string password = "");
+
+inline moodycamel::ReaderWriterQueue<std::string> input_queue;
+inline std::thread input_reader;
+inline std::unique_ptr<Rac2::Rac2_interface> rac2;
+
+inline bool rac2_connected;
+inline bool ap_connected;
